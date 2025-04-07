f13a55f88a90d10a77ae848f63a5ae84ed2a857d
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-04-07 15:28:19 +0200

    [/] kafka configuration

--- src/main/java/com/novus/authentication_service/configuration/SecurityConfiguration.java
+++ src/main/java/com/novus/authentication_service/configuration/SecurityConfiguration.java
@@ -60,9 +60,4 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
 }
