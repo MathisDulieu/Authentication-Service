b5c89334184d38370b0510200302e45a1ca3d722
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-04-07 16:06:26 +0200

    [/] kafka configuration

--- src/main/java/com/novus/authentication_service/configuration/KafkaConfiguration.java
+++ src/main/java/com/novus/authentication_service/configuration/KafkaConfiguration.java
@@ -36,7 +36,6 @@ public class KafkaConfiguration {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
 
         return new DefaultKafkaConsumerFactory<>(props);
     }
