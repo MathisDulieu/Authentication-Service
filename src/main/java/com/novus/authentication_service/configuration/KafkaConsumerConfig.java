16c92cf2cb08c1f8c9629da7431ba4464a2b9ff6
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-04-08 10:20:24 +0200

    [/] kafka configuration

--- src/main/java/com/novus/authentication_service/configuration/KafkaConsumerConfig.java
+++ src/main/java/com/novus/authentication_service/configuration/KafkaConsumerConfig.java
@@ -19,11 +19,11 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "${spring.kafka.consumer.bootstrap-servers}");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "${spring.kafka.consumer.group-id}");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka.railway.internal:29092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "${spring.kafka.consumer.auto-offset-reset}");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         return new DefaultKafkaConsumerFactory<>(props);
     }
