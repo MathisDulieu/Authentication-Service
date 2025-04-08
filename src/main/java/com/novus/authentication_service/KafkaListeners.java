8d34bd7509250e8e05ad356ce561ce441ca3853f
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-04-08 09:28:30 +0200

    [/] kafka configuration

---
+++ src/main/java/com/novus/authentication_service/KafkaListeners.java
@@ -0,0 +1,16 @@
+package com.novus.authentication_service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(
+            topics = "authenticationTopic",
+            groupId = "groupId"
+    )
+    void listener(String data) {
+        System.out.println("Date received : " + data);
+    }
+}
